3dde959493a83a10f802f8bb260e4d05a6955304
rabiulkhan7224 <mdrabiulkhanbabo@gmail.com>
2025-05-24 23:51:10 +0600

    mongodb connection done

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ app.use(express.json());
 
 
 
-// connectDB()
+connectDB()
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, World!');
 });
--- src/config/db.ts
+++ src/config/db.ts
@@ -2,8 +2,9 @@ import mongoose from "mongoose";
 
 const connectDB=async()=>{
     try {
-        const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/mydatabase';
-        await mongoose.connect(dbUrl,);
+        const dbUrl = process.env.MongoDB_URL
+        
+        await mongoose.connect(dbUrl as string);
         console.log('Database connected successfully');
     } catch (error) {
         console.error('Database connection failed:', error);
